e6d058011cbce43d2f511bd72794e28acbb68a0c
CaiqueLudemann <caiqueludemann@gmail.com>
2022-09-22 23:12:53 -0300

    testing navbar opacity on scroll event.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 //html elements
 const body = document.querySelector('body');
 //nav elements
-// const navBar = document.querySelector('.nav');
+const navBar = document.querySelector('.nav');
 const navList = document.querySelectorAll('.nav-li');
 const logo = document.getElementById('logo');
 const navInicio = document.getElementById('inicio');
@@ -16,21 +16,56 @@ const dropDownMenu = document.getElementById('drop-down');
 //==============================================
 
 
+//nav events&functions
+function navDarken() {
+  if (window.scrollY === 0) {
+    setTimeout(() => {
+      navBar.style.backgroundColor = "rgba(24, 23, 23, 0)"
+    }, 1600);
+    
+    
+  } else {
+    setTimeout(() => {
+      navBar.style.backgroundColor = "rgba(24, 23, 23, 0.2)";
+    }, 100);
+    setTimeout(() => {
+      navBar.style.backgroundColor = "rgba(24, 23, 23, 0.4)"
+    }, 200);
+    setTimeout(() => {
+      navBar.style.backgroundColor = "rgba(24, 23, 23, 0.6)"
+    }, 400);
+    setTimeout(() => {
+      navBar.style.backgroundColor = "rgba(24, 23, 23, 0.8)"
+    }, 800);
+  }
+};
+window.onscroll = navDarken;
+console.log(window.scrollY)
 
+
+
+// function navLighten() {
+//   if (document.body.scrollTop === 0) {
+//     setTimeout(() => {
+//       navBar.style.backgroundColor = "rgba(24, 23, 23, 0)";
+//     }, 300);
+//   };
+// };
+
+
+// window.onscroll = navLighten;
 
 
 function navLinkClicked() {
   navInicio.style.color = 'black';
   navInicio.style.backgroundColor = 'white';
-  // body.style.backgroundColor = 'white';
-}
+};
 
 function navLinkUnclicked() {
-  navInicio.style.color = ''
+  navInicio.style.color = '';
   navInicio.style.backgroundColor = '';
-  navInicio.style.borderRadius = '';
-  // body.style.backgroundColor = '';
-}
+};
+
 
 navInicio.onmouseover = navLinkClicked;
 navInicio.onmouseout = navLinkUnclicked;
@@ -43,26 +78,16 @@ function dropDown() {
     dropDownMenu.style.display = 'none';
     togglerButton.style.color = '';
     togglerButton.style.backgroundColor = '';
-    togglerButton.style.transform = 'rotate(0deg)'
+    togglerButton.style.transform = 'rotate(0deg)';
   } else {
     togglerButton.style.backgroundColor = 'white';
-    togglerButton.style.padding = '0.3em'
+    togglerButton.style.padding = '0.3em';
     togglerButton.style.borderRadius = '100%';
     togglerButton.style.transform = 'rotate(90deg)';
     togglerButton.style.color = 'black';
     dropDownMenu.style.borderRadius = '10px';
     dropDownMenu.style.display = 'block';
-  }
+  };
 };
 
 togglerButton.onclick = dropDown;
-
-
-
-
-
-
-
-
-
-
