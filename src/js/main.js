3e8daacab7c2bc3b0f9056f41a6946b255e237a3
CaiqueLudemann <caiqueludemann@gmail.com>
2022-09-27 22:48:08 -0300

    added calling text to home section.

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
-//html elements
+//section elements
 const body = document.querySelector('body');
 //nav elements
 const navBar = document.querySelector('.nav');
 const navList = document.querySelectorAll('.nav-li');
-const logo = document.getElementById('logo');
+const logo = document.querySelector('.logo');
+// const navLinks = document.querySelector('.navLink') //cant find how multiple elements
 const navInicio = document.getElementById('inicio');
 const navSobre = document.getElementById('sobre');
 const navMetodo = document.getElementById('metodo');
 const navPrecos = document.getElementById('precos');
-
-
 //toggler
 const togglerButton = document.getElementById('toggler-button');
-const dropDownMenu = document.getElementById('drop-down');
+const dropDownMenu = document.querySelector('.drop-down');
+//home elements
+//...
 //==============================================
 
 
@@ -21,49 +22,47 @@ function navDarken() {
   if (navBar.className === 'nav') {
     navBar.classList.add('navOnScroll');
     navBar.classList.remove('nav');
-    console.log(navBar.className);
+    logo.classList.add('logoOnScroll');
+    logo.classList.remove('logo');
+    navInicio.id = 'inicioOnScroll';
+    navSobre.id = 'sobreOnScroll';
+    navMetodo.id = 'metodoOnScroll';
+    navPrecos.id = 'precosOnScroll';
   }
   if (window.pageYOffset === 0 && navBar.className === 'navOnScroll') {
     navBar.classList.add('nav');
     navBar.classList.remove('navOnScroll');
-    console.log(navBar.className);
+    logo.classList.add('logo');
+    logo.classList.remove('logoOnScroll');
+    navInicio.id = 'inicio';
+    navSobre.id = 'sobre';
+    navMetodo.id = 'metodo';
+    navPrecos.id = 'precos';
+  }
+  if (togglerButton.id === 'toggler-buttonOnScroll' && window.pageYOffset === 0) {
+    togglerButton.id = 'toggler-button';
   }
 };
 window.onscroll = navDarken;
 
-function navLinkClicked() {
-  navInicio.style.color = 'black';
-  navInicio.style.backgroundColor = 'white';
-};
 
-function navLinkUnclicked() {
-  navInicio.style.color = '';
-  navInicio.style.backgroundColor = '';
-};
+//drop down menu
+togglerButton.addEventListener('click', function () {
+  if (togglerButton.id === 'toggler-button') {
+    togglerButton.id = 'toggler-buttonOnClick';
+    dropDownMenu.classList.add('drop-downOnClick');
+    dropDownMenu.classList.remove('drop-down');
 
+  }
+  else {
+    togglerButton.id = 'toggler-button';
+    dropDownMenu.classList.add('drop-down');
+    dropDownMenu.classList.remove('drop-downOnClick');
+  }
+});
 
-navInicio.onmouseover = navLinkClicked;
-navInicio.onmouseout = navLinkUnclicked;
-navInicio.onclick = navLinkClicked;
-
-
-
-
-function dropDown() {
-  if (dropDownMenu.style.display === 'block') {
-    dropDownMenu.style.display = 'none';
-    togglerButton.style.color = '';
-    togglerButton.style.backgroundColor = '';
-    togglerButton.style.transform = 'rotate(0deg)';
-  } else {
-    togglerButton.style.backgroundColor = 'white';
-    togglerButton.style.padding = '0.3em';
-    togglerButton.style.borderRadius = '100%';
-    togglerButton.style.transform = 'rotate(90deg)';
-    togglerButton.style.color = 'black';
-    dropDownMenu.style.borderRadius = '10px';
-    dropDownMenu.style.display = 'block';
-  };
-};
-
-togglerButton.onclick = dropDown;
+dropDownMenu.addEventListener('mouseleave', function () {
+  togglerButton.id = 'toggler-button';
+  dropDownMenu.classList.add('drop-down');
+  dropDownMenu.classList.remove('drop-downOnClick');
+})
